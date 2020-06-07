d82b0ded01efd5aedd0488c8a85f91e14bc4d049
Jorge Manzano <manzanoglez1995@gmail.com>
2020-06-07 18:56:26 -0500

    first commit

--- frontend/src/components/WsSubscriptions.js
+++ frontend/src/components/WsSubscriptions.js
@@ -6,6 +6,8 @@ import Vue from "vue";
 export default async () => {
   return new Promise((resolve, reject) => {
     Vue.ws.disconnect();
+
+    console.log(`ws://${window.location.host}`)
     Vue.ws.connect(
       {
         wsDomain: `ws://${window.location.host}`,
