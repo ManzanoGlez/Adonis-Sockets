870ef3cc9652cd73c5c98148342740d63ccc53b3
Jorge Manzano <manzanoglez1995@gmail.com>
2020-06-07 23:29:45 -0500

    first commit

--- frontend/src/components/WsSubscriptions.js
+++ frontend/src/components/WsSubscriptions.js
@@ -8,16 +8,16 @@ export default async () => {
 let protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
 let host = window.location.host;
 
-console.log(`${protocol}${host}`)
-console.log(`${protocol}://${host}/`)
+let wsDomain = `${protocol}://${host}/`;
+
 
   return new Promise((resolve, reject) => {
     Vue.ws.disconnect();
     Vue.ws.connect(
       {
-        wsDomain: `${protocol}${host}`,
+        wsDomain,
         jwtToken: null
-      }
+      },
     );
 
     Vue.ws.socket.on("open", () => { console.log("Conectado") });
