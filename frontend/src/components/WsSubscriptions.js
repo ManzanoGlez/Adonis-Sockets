63bfa8afd6d8a17179d1a6d4f472976f88c3520f
Jorge Manzano <manzanoglez1995@gmail.com>
2020-06-07 23:25:52 -0500

    first commit

--- frontend/src/components/WsSubscriptions.js
+++ frontend/src/components/WsSubscriptions.js
@@ -8,17 +8,15 @@ export default async () => {
 let protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
 let host = window.location.host;
 
+console.log(`${protocol}${host}`)
+console.log(`${protocol}://${host}/`)
+
   return new Promise((resolve, reject) => {
     Vue.ws.disconnect();
     Vue.ws.connect(
       {
         wsDomain: `${protocol}${host}`,
         jwtToken: null
-      },
-      {
-        path: "adonis-ws",
-        reconnectionAttempts: 300,
-        reconnectionDelay: 5000
       }
     );
 
